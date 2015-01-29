0f9a3b396e0d33309af01cc829934ce1e8bec616
djwalshy <the_walshs@att.net>
2015-01-29 16:00:46 -0600

    Update

--- Customer Relationship System/Customer Relationship System/Sales.Designer.vb
+++ Customer Relationship System/Customer Relationship System/Sales.Designer.vb
@@ -28,29 +28,41 @@ Partial Class Sales
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.lblEmail = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(28, 37)
+        Me.Label1.Location = New System.Drawing.Point(17, 27)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Customer Name:"
+        Me.Label1.Text = "Customer Number:"
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(119, 34)
+        Me.ComboBox1.Location = New System.Drawing.Point(114, 24)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
         Me.ComboBox1.TabIndex = 1
         '
         'lblPhoneNumber
         '
         Me.lblPhoneNumber.AutoSize = True
-        Me.lblPhoneNumber.Location = New System.Drawing.Point(31, 78)
+        Me.lblPhoneNumber.Location = New System.Drawing.Point(17, 101)
         Me.lblPhoneNumber.Name = "lblPhoneNumber"
         Me.lblPhoneNumber.Size = New System.Drawing.Size(81, 13)
         Me.lblPhoneNumber.TabIndex = 2
@@ -58,18 +70,17 @@ Partial Class Sales
         '
         'TextBox1
         '
-        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
-        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
         Me.TextBox1.Enabled = False
-        Me.TextBox1.Location = New System.Drawing.Point(118, 78)
+        Me.TextBox1.Location = New System.Drawing.Point(114, 101)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 13)
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 3
         '
         'lblEmail
         '
         Me.lblEmail.AutoSize = True
-        Me.lblEmail.Location = New System.Drawing.Point(31, 100)
+        Me.lblEmail.Location = New System.Drawing.Point(17, 125)
         Me.lblEmail.Name = "lblEmail"
         Me.lblEmail.Size = New System.Drawing.Size(35, 13)
         Me.lblEmail.TabIndex = 4
@@ -77,29 +88,123 @@ Partial Class Sales
         '
         'TextBox2
         '
-        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
-        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
         Me.TextBox2.Enabled = False
-        Me.TextBox2.Location = New System.Drawing.Point(119, 97)
+        Me.TextBox2.Location = New System.Drawing.Point(114, 124)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 13)
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
         Me.TextBox2.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Last Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "First Name:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(114, 77)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(114, 51)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 9
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.lblPhoneNumber)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.lblEmail)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(233, 158)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Customer Information"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox6)
+        Me.GroupBox2.Controls.Add(Me.TextBox5)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(276, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(228, 100)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Recent Sales"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Annual Sales:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(15, 53)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 13)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Montly Sales Avg:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(113, 50)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 2
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(113, 24)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 3
         '
         'Sales
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(538, 303)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.lblEmail)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.lblPhoneNumber)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(516, 189)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Sales"
         Me.Text = "Sales"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
@@ -108,4 +213,14 @@ Partial Class Sales
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents lblEmail As System.Windows.Forms.Label
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
--- Customer Relationship System/Customer Relationship System/Sales.vb
+++ Customer Relationship System/Customer Relationship System/Sales.vb
@@ -1,17 +1,22 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class Sales
+    Dim id As New List(Of String)
     Dim fnames As New List(Of String)
     Dim mnames As New List(Of String)
     Dim lnames As New List(Of String)
     Dim pnumber As New List(Of String)
     Dim email As New List(Of String)
+    Dim asales As New List(Of String)
+    Dim msales As New List(Of String)
+
 
 
     Private Sub Sales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim connstring As String = ("Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Bryan\Source\Repos\Customer-Relationship-System\Customer Relationship System\Customer Relationship System\Customers.mdf;Integrated Security=True;Connect Timeout=30")
         Dim conn As New SqlConnection(connstring)
         Dim cmd As New SqlCommand("SELECT * FROM Customer_Info", conn)
+        Dim cmd2 As New SqlCommand("SELECT * FROM Sales", conn)
 
 
 
@@ -20,6 +25,7 @@ Public Class Sales
             conn.Open()
             Dim rd As SqlDataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection)
             While rd.Read()
+                id.Add(rd("Id").ToString)
                 fnames.Add(rd("firstName").ToString)
                 mnames.Add(rd("middleName").ToString)
                 lnames.Add(rd("lastName").ToString)
@@ -29,8 +35,17 @@ Public Class Sales
             rd.Close()
             conn.Close()
 
+            conn.Open()
+            Dim rd2 As SqlDataReader = cmd2.ExecuteReader(CommandBehavior.CloseConnection)
+            While rd2.Read()
+                msales.Add(rd2("Monthly_Sales").ToString)
+                asales.Add(rd2("Annual_Sales").ToString)
+            End While
+            rd2.Close()
+            conn.Close()
+
             Me.ComboBox1.Items.Clear()
-            Me.ComboBox1.Items.AddRange(fnames.ToArray)
+            Me.ComboBox1.Items.AddRange(id.ToArray)
 
         Catch ex As Exception
             MessageBox.Show(e.ToString)
@@ -48,6 +63,10 @@ Public Class Sales
 
             TextBox1.Text = pnumber(curValue)
             TextBox2.Text = email(curValue)
+            TextBox4.Text = fnames(curValue)
+            TextBox3.Text = lnames(curValue)
+            TextBox6.Text = asales(curValue)
+            TextBox5.Text = msales(curValue)
         End If
 
     End Sub
